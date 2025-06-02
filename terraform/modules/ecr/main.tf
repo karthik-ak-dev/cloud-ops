869d30c63f9d9f6470a0b8d91cae987e4f6a171a
Karthik <karthik@Karthiks-MacBook-Pro.local>
2025-06-02 11:15:52 +0530

    Update/CD code & set ECR force delete

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "this" {
 
   name                 = "${var.project_name}-${each.key}"
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
